1a5eb80b239692e83fc0c8328118d4531086f09b
kunkuntang <kuntang@163.com>
2017-02-01 01:49:15 +0800

    finished the index page

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var path = require('path');
 
 //var serveStatic = require('serve-static');
 var bodyParser = require('body-parser');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 80;
 var app = express();
 
 app.set('views', './pages/views');
@@ -19,54 +19,23 @@ app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use('/img', express.static(path.join(__dirname, 'pages/img')));
 app.use('/css', express.static(path.join(__dirname, 'pages/css')));
-
+app.use('/js', express.static(path.join(__dirname, 'pages/js')));
 
 app.listen(port);
 
 console.log('imooc started on port ' + port);
 
-// admin addMovie
-app.post('/v1/add', function (req, res) {
-    var movie = req.body;
-    Movie.addMovie(movie, function (id) {
-        res.redirect('/about/' + id);
-    });
-});
-
-// admin update about
-app.post('/v1/update', function (req, res) {
-    var movie = req.body;
-
-    Movie.upadteMovie(movie, function (id) {
-        res.redirect('/about/' + id);
-    })
-
-});
-
-// admin delete about
-app.get('/v1/delete', function (req, res) {
-    var id = req.params.id;
-
-    Movie.deletMovie(id, function () {
-        res.redirect('/about/admin/list');
-    });
-});
-
 //index page
 app.get('/:page', function (req, res) {
     var page = req.params.page;
+    console.log('page' + page)
     res.render(page, {
         page: page
-    })
+    });
 });
 
 app.get('/', function (req, res) {
     res.render('index', {
         page: 'index'
-    })
+    });
 });
-
-
-app.get('/download/', function (req, res) {
-    res.download('./videos/' + req.query.movieName);
-})
